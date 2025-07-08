1721a2bdcb82d48532dc3b2f885dacc9590895f8
Claire Peng <clairepeng94@gmail.com>
2025-07-08 16:22:16 +0100

    add messy change for proof of concept

--- cypress/e2e/visual.cy.ts
+++ cypress/e2e/visual.cy.ts
@@ -1,5 +1,6 @@
 import "@percy/cypress";
 const URL = "https://p5js.org/";
+const LOCAL_URL = "http://localhost:4321/";
 
 Cypress.on("uncaught:exception", (err) => {
   if (err.message.includes("ResizeObserver loop completed")) {
@@ -10,7 +11,7 @@ Cypress.on("uncaught:exception", (err) => {
 describe("Integration test with visual testing", () => {
   it("Loads the homepage", () => {
     // Load the page or perform any other interactions with the app.
-    cy.visit(URL);
+    cy.visit(LOCAL_URL);
     // Take a snapshot for visual diffing
     cy.percySnapshot("Homepage responsive test", { widths: [768, 992, 1200] });
   });
